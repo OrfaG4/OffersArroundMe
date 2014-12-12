eebe21479442b1dfa72853115419ccae991de809
raniaway <raniaway@gmail.com>
2014-12-12 17:26:34 +0200

    Controllers/home

--- mvc/app/controllers/home.php
+++ mvc/app/controllers/home.php
@@ -2,24 +2,33 @@
 	class Home extends ControllerDAO{
 		
 		public function index(){
-			$offersModel = $this->model('OffersDAO');
-			$offers = $offersModel->getTopThreeOffersDAO();
+			session_start();
+			if(!isset($_SESSION['user'])){
+				$this->view('templates/header');
+				$this->view('login/index');
+				$this->view('templates/footer');
+			
+			}else{
+				$offersModel = $this->model('OffersDAO');
+				$offers = $offersModel->getTopThreeOffersDAO();
 				
-			$this->view('templates/header');
-			$this->view('home/index',array('offers'=>$offers));
-			$this->view('templates/footer');
+				$this->view('templates/header');
+				$this->view('home/index', array('offers'=>$offers));
+				$this->view('templates/footer');
+			}
 		}
 		
 		public function coor(){
-			$offersModel = $this->model('Offers');
+			$offersModel = $this->model('OffersDAO');
 			$coordinants = $offersModel->retreiveCoordinants();
 			$this->view('home/coordinants', array('coords'=>$coordinants));
 		}
 		
 		public function markers(){
-			$offersModel = $this->model('Offers');
+			$offersModel = $this->model('OffersDAO');
 			$markerData = $offersModel->getOffersDAO();
 			$this->view('home/markers', array('markers' => $markerData ));
 		}
+		
 	}
 ?>
