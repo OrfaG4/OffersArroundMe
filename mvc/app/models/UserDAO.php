a1f643fb161f8fad72634a6c27f42e451c9766c5
J0eLGK <GEORGE_TAME@HOTMAIL.COM>
2014-12-19 18:13:20 +0200

    Update UserDAO.php

--- mvc/app/models/UserDAO.php
+++ mvc/app/models/UserDAO.php
@@ -2,14 +2,34 @@
 	class UserDAO{
 		
 		protected $db;
-		
 		public function __construct(PDO $db){
 			$this->db = $db;
 		}
 				
-		public function getUserDAO(){
-			$user = $this->db->query("SELECT * FROM users");
+		public function getUsersDAO(){
+			$user = $this->db->query("SELECT * FROM users") or die(mysql_error());
 			return $user->fetchAll(PDO::FETCH_OBJ);
 		}
+				
+		public function auth($username, $password){
+                    $users = $this->getUsersDAO();
+                    foreach($users as $user){
+                        if($username == $user->username && $password == $user->password){
+                            //session_start();
+                            $_SESSION['user'] = $username;
+                            return true;
+			}
+                        else{
+                            return false;
+			}
+                    }
+		}
+		
+		public function  destroySession(){
+				//$_SESSION = array(); //destroy all of the session variables
+  				session_start();
+				session_unset();
+				session_destroy();
+			}
 	}
 ?>
