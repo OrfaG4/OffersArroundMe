b4ef91e0d659d868f25a5bbb8da0c927aaa1ec9d
J0eLGK <GEORGE_TAME@HOTMAIL.COM>
2014-12-19 18:12:38 +0200

    Update LocationDAO.php

--- mvc/app/models/LocationDAO.php
+++ mvc/app/models/LocationDAO.php
@@ -1,22 +1,17 @@
 <?php
-/*Η κλάση LocationDAO ειναι το model Που διαχειρίζεται
- *την τοποθεσία του χρήστη μεσα απο txt αρχείο
- */
-
 class LocationDAO{
 	protected $txt;
 	
 	//Διαβαζω το αρχείο και επιστρέφω τα περιεχόμενα του.
-	private function getMyLocation(){
-		$this->txt = fopen(APP . "location.txt", "r") or die("Unable to open file!");
+	public function getMyLocation($location){
+		$this->txt = fopen($location, "r") or die("Unable to open file!");
 		$context = fread($this->txt,filesize(APP . "location.txt"));
 		fclose($this->txt);
 		return $context;
 		}
 	
 	//Σπαω το κειμενο σε 2 μεταβλητές.	
-	public function seperateCoordinates(){
-		$context = $this->getMyLocation();
+	public function seperateCoordinates($context){
 		$coordinates = explode(" ",$context);
 		return $coordinates;
 	}
