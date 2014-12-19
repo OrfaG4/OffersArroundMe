3f0f17b9d1305a82502fc61816733a370813394f
J0eLGK <GEORGE_TAME@HOTMAIL.COM>
2014-12-19 18:11:47 +0200

    Update location.php

--- mvc/app/controllers/location.php
+++ mvc/app/controllers/location.php
@@ -3,10 +3,10 @@
 		
 		public function index(){
 			$locationModel = $this->model('LocationDAO');
-			$location = $locationModel->seperateCoordinates();
+                        $context = $locationModel->getMyLocation(APP . "location.txt");
+			$location = $locationModel->seperateCoordinates($context);
 			
 			$latLong = array("lat"=>$location[0],"long"=>$location[1]);
-			
 			$this->view('location/index',array('location'=>$latLong));
 		}
 	}
