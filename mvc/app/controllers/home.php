855150cffd1a65a98d9ea18b73af380b82e41b31
Georgios Orfanidis <OrfaG4@users.noreply.github.com>
2014-12-19 17:49:30 +0200

    Update home.php

--- mvc/app/controllers/home.php
+++ mvc/app/controllers/home.php
@@ -2,24 +2,33 @@
 	class Home extends ControllerDAO{
 		
 		public function index(){
-			$offersModel = $this->model('OffersDAO');
-			$offers = $offersModel->getTopThreeOffersDAO();
+			session_start();
+			if(!isset($_SESSION['user'])){
+				$this->view('templates/header');
+				$this->view('login/index');
+				$this->view('templates/footer');
+			
+			}else{
+				$offersModel = $this->model('OffersDAO');
+				$offers = $offersModel->getTopThreeOffersDAO("SELECT * FROM discounts ORDER BY percent DESC LIMIT 3 ");
 				
-			$this->view('templates/header');
-			$this->view('home/index',array('offers'=>$offers));
-			$this->view('templates/footer');
+				$this->view('templates/header');
+				$this->view('home/index', array('offers'=>$offers));
+				$this->view('templates/footer');
+			}
 		}
 		
 		public function coor(){
-			$offersModel = $this->model('Offers');
-			$coordinants = $offersModel->retreiveCoordinants();
-			$this->view('home/coordinants', array('coords'=>$coordinants));
+			$offersModel = $this->model('OffersDAO');
+			$location = $_POST['location'];
+			$coordinates = $offersModel->retreiveCoordinates($location,'www.google.com',80);
+			$this->view('home/coordinants', array('coords'=>$coordinates));
 		}
-		
 		public function markers(){
-			$offersModel = $this->model('Offers');
-			$markerData = $offersModel->getOffersDAO();
+			$offersModel = $this->model('OffersDAO');
+			$markerData = $offersModel->getOffersDAO("SELECT * FROM discounts");
 			$this->view('home/markers', array('markers' => $markerData ));
 		}
+		
 	}
 ?>
