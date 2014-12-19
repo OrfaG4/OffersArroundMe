bf3713f31610cab1f04707fd712c7e78e998358c
raniaway <raniaway@gmail.com>
2014-12-19 16:32:20 +0200

    controllerDao

--- mvc/app/core/ControllerDAO.php
+++ mvc/app/core/ControllerDAO.php
@@ -1,14 +1,14 @@
 <?php 
+require_once "Network.php";
 	class ControllerDAO{
 		
-		protected $db;
+		protected $db, $net;
 		
 		public function __construct(){
-			//$this->db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-			
 			try {
 				$this->db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 				$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$this->net = new Network();
 			} 
 			catch (PDOException $e){
 				return $e->getMessage();
@@ -18,7 +18,7 @@
 		
 		protected function model($model){
 			require_once '../app/models/' . $model .'.php';
-			return new $model($this->db);
+			return new $model($this->db, $this->net);
 		}
 		
 		protected function view($view, $data=array()){
