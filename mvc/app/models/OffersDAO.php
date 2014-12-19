b3a6e33b482c20a4ee1d7b38f8a52ebc4fb1e71b
J0eLGK <GEORGE_TAME@HOTMAIL.COM>
2014-12-19 18:13:01 +0200

    Update OffersDAO.php

--- mvc/app/models/OffersDAO.php
+++ mvc/app/models/OffersDAO.php
@@ -1,34 +1,41 @@
 <?php 
 	class OffersDAO{
-		protected $db;
+		protected $db, $net;
 		
-		public function __construct(PDO $db){
+		public function __construct(PDO $db, Network $net){
 			$this->db = $db;
+			$this->net = $net;
 		}
 		
-		public function retreiveCoordinants(){
-				if(isset($_POST['location'])){
-				$location = $_POST['location'];
-				$url = "http://maps.google.com/maps/api/geocode/json?address=$location&sensor=false&region=GR";
-				$response = file_get_contents($url);
-				$response = json_decode($response, true);
-							
-				$lat = $response['results'][0]['geometry']['location']['lat'];
-				$long = $response['results'][0]['geometry']['location']['lng'];
-							
-				$coordinants = array("lat"=>$lat, "long"=>$long);
-				return $coordinants;
-			}
-		}
+		public function retreiveCoordinates($location, $confirmConnectionDomain, $confirmConnectionPort){
+                    $connection = $this->net->determineInternetConnection($confirmConnectionDomain, $confirmConnectionPort);
+                    if($connection){
+                        $response = $this->net->getGoogleDataFor($location, $connection);
+			$lat = null;
+                        $long = null;
+                        $jsonArray = json_decode($response, true);
+			if($jsonArray['results']!=null){
+                            $lat = $jsonArray['results'][0]['geometry']['location']['lat'];
+                            $long = $jsonArray['results'][0]['geometry']['location']['lng'];
+                        }		
+                    $coordinates = array("lat"=>$lat, "long"=>$long);
+                    return $coordinates;
+                    }
+                    else{
+			return array('error'=>'No Internet');
+                    }
+						
+                }
+		
 		//epistrefei oles tis prosfores		
-		public function getOffersDAO(){
-			$offers = $this->db->query("SELECT * FROM discounts")or die(mysql_error());
+		public function getOffersDAO($sql){
+			$offers = $this->db->query($sql)or die(mysql_error());
 			return $offers->fetchAll(PDO::FETCH_OBJ);
 		}
 		
 		//epistrefei tis korufaies 3 prosfores
-		public function getTopThreeOffersDAO(){
-			$topThreeOffers = $this->db->query("SELECT * FROM discounts ORDER BY percent DESC LIMIT 3 ")or die(mysql_error());
+		public function getTopThreeOffersDAO($sql){
+			$topThreeOffers = $this->db->query($sql)or die(mysql_error());
 			return $topThreeOffers->fetchAll(PDO::FETCH_OBJ);
 		}
 	}
