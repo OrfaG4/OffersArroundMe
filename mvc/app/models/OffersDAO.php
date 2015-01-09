912e1fa335eb0406f79ce7c449f47f91015327e2
J0eLGK <GEORGE_TAME@HOTMAIL.COM>
2015-01-09 16:22:08 +0200

    new models
    update add usersideoffers

--- mvc/app/models/OffersDAO.php
+++ mvc/app/models/OffersDAO.php
@@ -17,14 +17,17 @@
 			if($jsonArray['results']!=null){
                             $lat = $jsonArray['results'][0]['geometry']['location']['lat'];
                             $long = $jsonArray['results'][0]['geometry']['location']['lng'];
+                            
+                            if(isset($_POST['distance'])){
+                                $distance = $_POST['distance'];
+                            }
                         }		
-                    $coordinates = array("lat"=>$lat, "long"=>$long);
+                    $coordinates = array("lat"=>$lat, "long"=>$long,"distance"=>$distance);
                     return $coordinates;
                     }
                     else{
 			return array('error'=>'No Internet');
-                    }
-						
+                    }			
                 }
 		
 		//epistrefei oles tis prosfores		
@@ -38,5 +41,27 @@
 			$topThreeOffers = $this->db->query($sql)or die(mysql_error());
 			return $topThreeOffers->fetchAll(PDO::FETCH_OBJ);
 		}
+                
+                public function getOffersByUser($uid){
+                    $sql = "SELECT * FROM discounts WHERE user_id = '$uid'";
+                    $offers = $this->db->query($sql)or die(mysql_error());
+                    return $offers->fetchAll(PDO::FETCH_OBJ);
+                }
+                
+                public function updateOfferDetails($id, $title, $desc, $lat, $long, $percent){
+                    $sql = "UPDATE discounts SET title = '$title', `desc` = '$desc', lat = '$lat', `long` = '$long', percent = '$percent' WHERE id = '$id'";
+                    $offers = $this->db->query($sql)or die(mysql_error());
+                    if($offers) return true;
+                }
+                public function addOffer($title, $uid){
+                    $sql="INSERT INTO discounts (title, user_id) VALUES ('$title', '$uid')";
+                    $newOffer = $this->db->query($sql)or die(mysql_error());
+                    if($newOffer) return true;
+                }
+                public function deleteOffer($title, $uid){
+                    $sql="DELETE FROM discounts WHERE title = '$title' AND user_id = '$uid'";
+                    $deletedOffer = $this->db->query($sql)or die(mysql_error());
+                    if($deletedOffer) return true;  
+                }
 	}
 ?>
