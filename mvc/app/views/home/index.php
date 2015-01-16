74cfc47d96c71410f6636e8eeda0d760e590b54a
dimitrisKK <jim_karad@hotmail.gr>
2015-01-16 18:09:09 +0200

    Update index.php

--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -5,7 +5,7 @@
 			 <?php if(isset($_SESSION['user'])){ 
 				 echo"<div class='row'>";
     			 echo"<div class='col-md-2 col-md-offset-5 jumbotron'>";
-				 echo"<h3 style='text-align:center'>Welcome back " . $_SESSION['user'] . " !</h3>";
+				 echo"<h3 style='text-align:center'>Weclome back " . $_SESSION['user'] . " !</h3>";
 				 echo"<form action=" . HOME_PAGE . "/logout>";
 				 echo "<div class='col-md-2 col-md-offset-4 controls'>
 						  <input type='submit' id='btn-login' class='btn btn-success' value='Logout'  />
@@ -17,12 +17,19 @@
 			?>
 
 		<div class="col-md-8">
-		<form id="searchForm" method="POST">
-			<h1 class="jumbotron">Δώστε την τοποθεσια σας! &nbsp&nbsp
-			<input type="text" placeholder="Search..." name = "location"/> 
-			<button type="submit" class="btn btn-primary" id="searchBtn">Αναζήτηση</button></h1>
+		<form id="searchForm" method="POST" class="jumbotron">
+			<h2>What place to search? &nbsp&nbsp
+			<input type="text" placeholder="Search..." name = "location"/>
+                        <button type="submit" class="btn btn-primary" id="searchBtn">Αναζήτηση</button></h2>
+                        <label>Radius of offers in km: </label>
+                        <select name="distance">
+                            <option selected="selected">All Available</option>
+                            <option>0.1</option>
+                            <option>0.3</option>
+                            <option>1</option>
+                        </select>
 		</form>
-			<div id="googleMap" class="googleMap"></div>
+                <div id="googleMap" class="googleMap"></div>
 		</div>
 		<div class="col-md-offset-1 col-md-3" id="response">
         	<h1 class="jumbotron">Top 3 Προσφορές</h1>
@@ -33,6 +40,6 @@
 					echo "<p class='h4'>".$offer->desc. "</p><br />";
 				echo "</section>";
 				}?>
-        </div>
+                 </div>
 	</div>
 	<!-- Κυριο Περιεχόμενο Τελος -->
